bbf50691ed8520d7b9b5e687be38b41e2241323e
Galen Long <nlong@smith.edu>
2014-10-25 14:55:40 -0400

    Limited panning boundaries

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ViewController.swift 
 //  MapApp
 //
 //  Created by Galen Long on 10/19/14.
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var mapView: UIImageView!
+    
+    // set panning boundaries
+    var topBoundary: CGFloat = 0
+    var leftBoundary: CGFloat = 0
+    var rightBoundary: CGFloat = 375
+    var bottomBoundary: CGFloat = 710
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,17 +30,40 @@ class ViewController: UIViewController {
     
     @IBAction func handlePan(recognizer:UIPanGestureRecognizer) {
         let translation = recognizer.translationInView(self.view)
-        recognizer.view!.center = CGPoint(x:recognizer.view!.center.x + translation.x,
-            y:recognizer.view!.center.y + translation.y)
-        recognizer.setTranslation(CGPointZero, inView: self.view)
-        let mapImage = mapView.image
         
+        var desiredCenter = CGPoint(x:recognizer.view!.center.x + translation.x,
+            y:recognizer.view!.center.y + translation.y)
+        
+        // coordinates of current position relative to superview
+        var topLeftX: CGFloat = mapView.frame.origin.x
+        var topLeftY: CGFloat = mapView.frame.origin.y
+        var bottomRightX: CGFloat = mapView.frame.origin.x + mapView.frame.width
+        var bottomRightY: CGFloat = mapView.frame.origin.y + mapView.frame.height
+        
+        // limit panning to screen bounaries
+        if (topLeftX + translation.x < leftBoundary
+            && topLeftY + translation.y < topBoundary
+            && bottomRightX + translation.x > rightBoundary
+            && bottomRightY + translation.y > bottomBoundary)
+        {
+            recognizer.view!.center = desiredCenter
+            println("ok!")
+        } else {
+            println("oh no!")
+        }
+        
+        // don't compound translation with multiple calls for same pan
+        recognizer.setTranslation(CGPointZero, inView: self.view)
     }
     
     @IBAction func handlePinch(recognizer : UIPinchGestureRecognizer) {
         recognizer.view!.transform = CGAffineTransformScale(recognizer.view!.transform,
             recognizer.scale, recognizer.scale)
+        
+        // todo: limit pinching
+        // todo: automatically move image back to within boundaries if pinching would move it out
+        
+        // don't compound scaling with multiple calls for same pinch
         recognizer.scale = 1
-
     }
 }
